af83280502e2a4f917dc212f7f760d647a0e07cd
Michael <michaelx@oriental.com.au>
2021-12-13 16:19:10 +1100

    posts public to all

--- src/main/java/com/example/crappostsbackend/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/example/crappostsbackend/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.example.crappostsbackend.filter.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,7 +33,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests().antMatchers("/api/v1/users/login", "/api/v1/users/register").permitAll()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST, "/api/v1/users/register", "/api/v1/users/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/posts").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
